1745df9eee960a307475ec486b5aee4b877b1dd3
Serhii <ry6ens@gmail.com>
2024-12-23 13:15:36 +0200

    Update

--- src/app/auth/callback/route.ts
+++ src/app/auth/callback/route.ts
@@ -13,13 +13,21 @@ export async function GET(request: Request) {
     if (!error) {
       const forwardedHost = request.headers.get('x-forwarded-host')
       const isLocalEnv = process.env.NODE_ENV === 'development'
+
       if (isLocalEnv) {
+        console.log('isLocalEnv', isLocalEnv)
+        console.log('origin-next', `${origin}${next}`)
+
         return NextResponse.redirect(`${origin}${next}`)
       }
       if (forwardedHost) {
+        console.log('forwardedHost', forwardedHost)
+        console.log('origin-next', `${forwardedHost}${next}`)
+
         return NextResponse.redirect(`https://${forwardedHost}${next}`)
       }
 
+      console.log('origin-next', `${origin}${next}`)
       return NextResponse.redirect(`${origin}${next}`)
     }
   }
